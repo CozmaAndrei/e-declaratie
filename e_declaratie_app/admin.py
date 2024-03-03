dcfde22af79df8d82f1f093f3896cd754371f40f
CozmaAndrei <cozma.andrei91@gmail.com>
2024-03-03 14:06:31 +0200

    admin.py modified

--- e_declaratie_app/admin.py
+++ e_declaratie_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from user_company_app.models import Company
-from user_app.models import ExtraUserInformations
 
 '''Unregister the Group model from the admin interface, as we won't be using it.'''
 admin.site.unregister(Group)
@@ -9,6 +8,5 @@ admin.site.unregister(Group)
 '''Register Company model in admin interface'''
 admin.site.register(Company)
 
-'''Register ExtraUserInformations in admin interface'''
-admin.site.register(ExtraUserInformations)
+
 
--- user_app/admin.py
+++ user_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ExtraUserInformations
 
-# Register your models here.
+'''Register ExtraUserInformations in admin interface'''
+admin.site.register(ExtraUserInformations)
