476af9fbbc495379f8e62c9e116b92436538e883
CozmaAndrei <cozma.andrei91@gmail.com>
2024-03-07 12:59:53 +0200

    added follow/unfollow button in user view profile

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('profile/<str:username>/change-password/', views.change_pass, name='change_pass'),
     path('delete-user/<int:user_id>/', views.delete_user_account, name="delete-user-account"),
     path('users-lists/', views.users_lists, name='users_lists'),
-    path('users-list/<str:username>/', views.user_view_profile, name='user_view_profile'),
+    path('users-list/profile/<str:username>/', views.user_view_profile, name='user_view_profile'),
     
 ]
--- users/views.py
+++ users/views.py
@@ -61,9 +61,17 @@ def users_lists(request):
 
 '''Return the user view profile in viewprofilepage.html for all the users'''
 def user_view_profile(request, username):
-    view_user = User.objects.get(username=username) #used in viewprofilepage.html
-    extra_view_user_info = ExtraUserInformations.objects.get(user=view_user) #request in ExtraUserInformations model for table fields
-    return render(request, 'user_html/viewprofilepage.html', {"view_user": view_user, "extra_view_user_info": extra_view_user_info})
+    view_user = User.objects.get(username=username)
+    extra_view_user_info = ExtraUserInformations.objects.get(user=view_user)
+    current_user_profile = request.user.extrauserinformations  # Get the ExtraUserInformations instance for the current user
+    if request.method == "POST":
+        action = request.POST.get("follow")
+        if action == "unfollow":
+            current_user_profile.favorite_user.remove(view_user)
+        elif action == "follow":
+            current_user_profile.favorite_user.add(view_user)
+        current_user_profile.save()
+    return render(request, 'user_html/viewprofilepage.html', {"view_user": view_user, "extra_view_user_info": extra_view_user_info, "current_user_profile": current_user_profile})
 
 '''Change the user password'''
 def change_pass(request,username):
