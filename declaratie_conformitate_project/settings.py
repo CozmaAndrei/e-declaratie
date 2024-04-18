3d76eed2f119d47e42fb1089b92f908bb930bb96
CozmaAndrei <cozma.andrei91@gmail.com>
2024-04-18 13:15:08 +0300

    gitignore

--- declaratie_conformitate_project/settings.py
+++ declaratie_conformitate_project/settings.py
@@ -176,8 +176,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_FROM = 'edeclaratie.contact@gmail.com'
-EMAIL_HOST_USER = 'edeclaratie.contact@gmail.com'
+EMAIL_FROM = config('EMAIL_FROM')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
