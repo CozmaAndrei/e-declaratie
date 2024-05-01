5f1f8b27f3335a42e3349a19513cd43664f9dbc0
Cozma Andrei <cozma.andrei91@gmail.com>
2024-05-01 11:20:43 +0300

    added more style on website

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -4,8 +4,8 @@ from django.core.validators import EmailValidator
 class ContactForm(forms.Form):
     
     contact_widget_form = {'class': 'form-control', 'rows': 6, 'cols': 100,
-                        'style': 'box-shadow: 0 0 05px rgba(0, 0, 0, 0.5);' 'resize:none; padding: 10px;', 
-                        'onfocus': 'this.style.borderColor="grey";', 
+                        'style': 'box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);' 'resize:none; padding: 10px;', 
+                        'onfocus': 'this.style.borderColor="#019cbb";', 
                         'onfocusout': 'this.style.borderColor="";'
                     }
     name = forms.CharField(label='Nume', max_length=100, widget=forms.TextInput(attrs=contact_widget_form))
