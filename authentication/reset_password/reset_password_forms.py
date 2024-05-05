caf79854b66754ab63e7bf5e5ef417939e8a2029
Cozma Andrei <cozma.andrei91@gmail.com>
2024-05-05 16:12:52 +0300

    modify the reset pass style

--- authentication/reset_password/reset_password_forms.py
+++ authentication/reset_password/reset_password_forms.py
@@ -8,7 +8,7 @@ class CustomResetPasswordForm(PasswordResetForm):
         super().__init__(*args, **kwargs)
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['size'] = '30'
-        self.fields['email'].widget.attrs['style'] = 'box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);'
+        self.fields['email'].widget.attrs['style'] = 'box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);'
         self.fields['email'].widget.attrs['onfocus'] = 'this.style.borderColor="#019cbb";'
         self.fields['email'].widget.attrs['onfocusout'] = 'this.style.borderColor="";'
     
@@ -20,8 +20,8 @@ class CustomSetPasswordForm(SetPasswordForm):
         self.fields['new_password2'].widget.attrs['class'] = 'form-control'
         self.fields['new_password1'].widget.attrs['size'] = '30'
         self.fields['new_password2'].widget.attrs['size'] = '30'
-        self.fields['new_password1'].widget.attrs['style'] = 'box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);'
-        self.fields['new_password2'].widget.attrs['style'] = 'box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);'
+        self.fields['new_password1'].widget.attrs['style'] = 'box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);'
+        self.fields['new_password2'].widget.attrs['style'] = 'box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);'
         self.fields['new_password1'].widget.attrs['onfocus'] = 'this.style.borderColor="#019cbb";'
         self.fields['new_password1'].widget.attrs['onfocusout'] = 'this.style.borderColor="";'
         self.fields['new_password2'].widget.attrs['onfocus'] = 'this.style.borderColor="#019cbb";'
--- create_declaration/forms.py
+++ create_declaration/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class MyForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs={'id': 'myTextarea','class': 'form-control', 'rows': 8, 'style': 'resize:none; padding: 10px;'}))
+    text = forms.CharField(widget=forms.Textarea(attrs={'id': 'myTextarea',
+                                                        'class': 'form-control',
+                                                        'rows': 6,
+                                                        'cols': 2,
+                                                        'style': 'padding: 10px;'
+                                                        }))
